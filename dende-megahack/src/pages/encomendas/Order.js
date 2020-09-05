c72a2e57dd075c1f7b0b9f60030a3c9b195ae38e
RenataNovais <rnovaiscs@gmail.com>
2020-09-05 16:34:41 -0300

    tela de cadastro de encomenda - falta funcionalidades

--- dende-megahack/src/pages/encomendas/Order.js
+++ dende-megahack/src/pages/encomendas/Order.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import ProductInput from '../../components/ProductInput';
@@ -6,10 +6,10 @@ import Header from '../../components/Header';
 import Button from 'react-bootstrap/esm/Button';
 
 function Order() {
-
-  // const addNew = () => { 
-
-  // }
+  const [client, setClient] = useState();
+  const [date, setDate] = useState();
+  const [delivery, setDelivery] = useState();
+  const [details, setDetails] = useState();
 
   return (
     <div>
@@ -21,13 +21,26 @@ function Order() {
           <Button className="button-verde">
             + produto
           </Button>
-          <Form.Control placeholder="Nome do Cliente" />
-          <Form.Control type="date" />
-          <Form.Row>
-            <Form.Check type="radio" id="entregar" name="entregar" label="Entregar" />
-            <Form.Check type="radio" id="buscar" name="buscar" label="Buscar" />
-          </Form.Row>
-          <Form.Control as="textarea" rows="4" placeholder="Detalhes ou mais informações" />
+          <Form.Group>
+            <Form.Label>Cliente</Form.Label>
+            <Form.Control placeholder="Nome do Cliente" onChange={(e) => setClient(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Prazo</Form.Label>
+            <Form.Control type="date" onChange={(e) => setDate(e.target.value)} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Escolha a opção de entrega</Form.Label>
+            <Form.Control as="select">
+              <option value="">Selecione</option>
+              <option value="entregar">Entregar</option>
+              <option value="buscar">Buscar</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Mais Informações</Form.Label>
+            <Form.Control as="textarea" rows="4" placeholder="Detalhes ou mais informações" />
+          </Form.Group>
           <Button className="button-verde">
             Agendar
           </Button>
