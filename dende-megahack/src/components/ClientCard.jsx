210344edf523a7b877992a0352d3524fe307eadd
marinabrcls <barcelos.marina@gmail.com>
2020-09-05 14:05:50 -0300

    client card base

---
+++ dende-megahack/src/components/ClientCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ClientCard() {
+  return (
+    <div>
+      <h1>Client Name</h1>
+      <p>Phone number</p>
+      <p>Address</p>
+    </div>
+  )
+};
+
+export default ClientCard;
