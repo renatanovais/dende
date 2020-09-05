45281ea4be995c74b75186e20cd6766a2569ef8b
marinabrcls <barcelos.marina@gmail.com>
2020-09-05 14:46:23 -0300

    comentando pra não quebrar o code

--- dende-megahack/src/pages/Clients.js
+++ dende-megahack/src/pages/Clients.js
@@ -4,22 +4,22 @@ import { connect } from 'react-redux';
 // se os dados viessem de uma API própria
 // teria um useEffect(() => getClientsFromAPI, [])
 
-function Clients(props) {
-  const { clients } = props;
+export function Clients(props) {
+//  const { clients } = props;
   return (
     <div>
       <h1>Lista de Clientes</h1>
-      <div>
+{/*       <div>
         {clients.map((client) => (
           <ClientCard key={client.name} />
         ))}
-      </div>
+      </div> */}
     </div>
   )
 };
 
-const mapStateToProps = (state) => ({
+/* const mapStateToProps = (state) => ({
   clients: state.clientsReducer.clients
-});
+}); */
 
-export default connect(mapStateToProps)(Clients);
+// export default connect(mapStateToProps)(Clients);
