a667ebe3757b9a4bd6a63fee4a8124947b6646cb
RenataNovais <rnovaiscs@gmail.com>
2020-09-06 01:18:41 -0300

    link pra home no header

--- dende-megahack/src/components/Header.jsx
+++ dende-megahack/src/components/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import logoHorizontal from '../images/logos/logo_dende_horizontal_laranja.png';
 import configs from '../images/icons/configs-icon.png';
+import { Link } from 'react-router-dom';
 
 function Header(props) {
   return (
     <header className="header-comp">
       <div>
-        <img alt="Dendê" src={logoHorizontal} height="90px" />
+        <Link to="/home">
+          <img alt="Dendê" src={logoHorizontal} height="90px" />
+        </Link>
       </div>
       <div className="header-configs">
         <p>{props.email}</p>
