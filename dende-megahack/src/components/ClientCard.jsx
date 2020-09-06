5c964ded108f4db84deaa4d3fdf3867a06f8266b
marinabrcls <barcelos.marina@gmail.com>
2020-09-05 21:40:53 -0300

    base do client card feita

--- dende-megahack/src/components/ClientCard.jsx
+++ dende-megahack/src/components/ClientCard.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function ClientCard() {
+function ClientCard(props) {
+  const { name, birthday, phone, email, address, registryDate } = props;
   return (
     <div>
-      <h1>Client Name</h1>
-      <p>Phone number</p>
-      <p>Address</p>
+      <h1>{name}</h1>
+      <p>Aniversário: {birthday}</p>
+      <p>Telefone: {phone}</p>
+      <p>E-mail: {email}</p>
+      <p>Endereço: {address}</p>
+      <p>Data de cadastro:{registryDate}</p>
     </div>
-  )
-};
+  );
+}
 
-export default ClientCard;
+const mapStateToProps = (state) => ({
+  name: state.clientsReducer.client.name,
+  birthday: state.clientsReducer.client.birthday,
+  phone: state.clientsReducer.client.phone,
+  email: state.clientsReducer.client.email,
+  address: state.clientsReducer.client.address,
+  registryDate: state.clientsReducer.client.registryDate,
+});
+
+export default connect(mapStateToProps)(ClientCard);
