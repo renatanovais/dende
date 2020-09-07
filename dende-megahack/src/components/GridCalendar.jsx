47c8d6ce84aaf64c8b2168e9b1b8951112569758
RenataNovais <rnovaiscs@gmail.com>
2020-09-06 23:35:25 -0300

    tirando warnings de key nos maps, tudo com i (index). sorry CC

--- dende-megahack/src/components/GridCalendar.jsx
+++ dende-megahack/src/components/GridCalendar.jsx
@@ -10,7 +10,7 @@ export default function GridCalendar(props) {
       <Card.Body>
         <Card.Title className="client-name">{order.client}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{`${order.delivery}: ${dateFormat[2]} / ${dateFormat[1]} / ${dateFormat[0]}`}</Card.Subtitle>
-        {order.products.map((product) => <ListGroupItem className="list-item">{product.product} - {product.quantity} unid.</ListGroupItem>)}
+        {order.products.map((product, i) => <ListGroupItem key={i} className="list-item">{product.product} - {product.quantity} unid.</ListGroupItem>)}
         <Card.Text className="order-details">
           <p>Detalhes:</p>
           <span>{order.details}</span>
